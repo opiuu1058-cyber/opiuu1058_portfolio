8424dba72d60cf3c42b65e845a0ce5f284211370
unknown <opiuu1058@ha.ac.kr>
2025-10-28 16:35:25 +0900

    Restructure project for GitHub Pages

--- setup_and_commit.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-"C:\Program Files\Git\bin\git.exe" config user.name "박다찬"
-"C:\Program Files\Git\bin\git.exe" config user.email "opiuu1058@ha.ac.kr"
-"C:\Program Files\Git\bin\git.exe" commit -m "Initial commit"
