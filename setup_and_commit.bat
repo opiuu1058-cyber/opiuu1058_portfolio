571fd2bd1cd0a53da07f650d0d2126540c59c278
unknown <opiuu1058@ha.ac.kr>
2025-10-28 16:21:55 +0900

    Change logo text from BiCute to HANSHIN

---
+++ setup_and_commit.bat
@@ -0,0 +1,4 @@
+@echo off
+"C:\Program Files\Git\bin\git.exe" config user.name "박다찬"
+"C:\Program Files\Git\bin\git.exe" config user.email "opiuu1058@ha.ac.kr"
+"C:\Program Files\Git\bin\git.exe" commit -m "Initial commit"
